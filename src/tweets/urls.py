bf19b1294151177f208ddd6f391e114c2c138ec9
sasiliyu <sasil.adetunji@gmail.com>
2018-04-24 08:24:58 +0100

    adds dynamic url

--- src/tweets/urls.py
+++ src/tweets/urls.py
@@ -1,9 +1,11 @@
-from .views import tweet_detail_view, tweet_list_view
+from .views import TweetDetailView, TweetListView
 from django.conf.urls import url
 
 urlpatterns = [
     #url(r'^admin/', admin.site.urls),
-    url(r'^$', tweet_list_view, name='list'),
-    url(r'^1/$', tweet_detail_view, name='detail'),
+    url(r'^$', TweetListView.as_view(), name='list'),
+    url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'),
+    # url(r'^(?P<pk>\d+)/$', tweet_detail_view, name='detail'),
+
 
 ]

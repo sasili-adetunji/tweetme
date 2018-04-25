a88f18d5fd209d35c3669cef1d640567c3531593
sasiliyu <sasil.adetunji@gmail.com>
2018-04-25 12:17:30 +0100

    add bootsrp navbar

--- src/tweetme/settings/local.py
+++ src/tweetme/settings/local.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/some-static/'
+STATIC_URL = '/static/'
 
 #will not be serve but will be store
 STATICFILES_DIRS = [

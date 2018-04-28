cea1a65b22df2bb51b1b4af4ab819645286a6465
sasiliyu <sasil.adetunji@gmail.com>
2018-04-28 14:24:21 +0100

    add jquery

--- src/tweets/api/views.py
+++ src/tweets/api/views.py
@@ -1,9 +1,20 @@
 from .serializers import TweetModelSerializer
 from rest_framework import generics
 from tweets.models import Tweet
+from django.db.models import Q
+
 
 class TweetListAPIView(generics.ListAPIView):
     serializer_class = TweetModelSerializer
 
-    def get_queryset(self):
-      return Tweet.objects.all()
+    def get_queryset(self, *args, **kwargs):
+
+        queryset = Tweet.objects.all()
+        print(self.request.GET)
+        query = self.request.GET.get('q', None)
+        if query is not None:
+            queryset = queryset.filter(
+                Q(content__icontains=query) |
+                Q(user__username__icontains=query)
+                )
+        return queryset
